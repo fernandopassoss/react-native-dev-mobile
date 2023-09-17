f8a6256d9418967f673b8e8fa7e9ac5b485a67d5
Fernando <73785564+fernandopassoss@users.noreply.github.com>
2023-09-17 13:00:14 -0300

    Add files via upload

--- projeto-react-native/App.js
+++ projeto-react-native/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import { Container, Header, Content, H1, Text, Button } from 'native-base';
-import ProductList from './ProductList';
+import { Container, Header, Content, Button, Text } from 'native-base';
 import Rating from './Rating';
-
-
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
 export default function App() {
-  const user = {
-    firstName: 'fefe',
-    lastName: 'passos'
-  };
-  const isValid = true;
-
   return (
     <Container>
       <Header />
       <Content>
-        <Button disabled={!isValid} >
-          <Text>Clique aqui</Text>
-        </Button>
-        <Button danger>
-          <Text>Atenção</Text>
-        </Button>
         <Rating rating='1' />
         <Rating rating='2' />
         <Rating rating='3' />
